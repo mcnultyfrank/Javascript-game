696e830f23dd4aeb9e774a399599b1344bf0be70
Frank McNulty <frank.mcnulty@nology.io>
2020-09-30 20:24:22 +0100

    menu edits

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -1,6 +1,47 @@
 "use strict";
 
-var player = document.querySelector(".player");
-player.addEventListener("click", function (e) {
-  e.target.classList.add("player-jump");
+var playerJump = document.querySelector(".player");
+var screenClick = document.querySelector(".game-container");
+var easyButton = document.querySelector(".easy");
+var mediumButton = document.querySelector(".medium");
+var hardButton = document.querySelector(".hard");
+var startButtonBlur = document.querySelector(".start__button_blur");
+var startButtonHover = document.querySelector(".start__button");
+var startButton = document.querySelector(".start__button");
+var Menu = document.querySelector(".menu-container");
+easyButton.addEventListener("click", function (e) {
+  easyButton.classList.add("selected__difficulty");
+  startButtonBlur.classList.remove("start__button_blur");
+  mediumButton.classList.remove("selected__difficulty");
+  hardButton.classList.remove("selected__difficulty");
+  startButtonHover.classList.add("start__button__hover");
+});
+mediumButton.addEventListener("click", function (e) {
+  mediumButton.classList.add("selected__difficulty");
+  startButtonBlur.classList.remove("start__button_blur");
+  easyButton.classList.remove("selected__difficulty");
+  hardButton.classList.remove("selected__difficulty");
+  startButtonHover.classList.add("start__button__hover");
+});
+hardButton.addEventListener("click", function (e) {
+  hardButton.classList.add("selected__difficulty");
+  startButtonBlur.classList.remove("start__button_blur");
+  easyButton.classList.remove("selected__difficulty");
+  mediumButton.classList.remove("selected__difficulty");
+  startButtonHover.classList.add("start__button__hover");
+});
+startButton.addEventListener("click", function (e) {
+  // if(easyButton.classList == "selected__difficulty")
+  Menu.classList.add("start__menu__no__display");
+  console.log('hi');
+  screenClick.addEventListener("click", function (e) {
+    if (playerJump.classList == "player-jump") {
+      return;
+    }
+
+    playerJump.classList.add("player-jump");
+    setTimeout(function () {
+      playerJump.classList.remove("player-jump");
+    }, 900);
+  });
 });
--- script.js
+++ script.js
@@ -1,6 +1,46 @@
-const player = document.querySelector(".player");
+const playerJump = document.querySelector(".player");
+const screenClick = document.querySelector(".game-container");
+const easyButton = document.querySelector(".easy");
+const mediumButton = document.querySelector(".medium");
+const hardButton = document.querySelector(".hard");
+const startButtonBlur = document.querySelector(".start__button_blur");
+const startButtonHover = document.querySelector(".start__button");
+const startButton = document.querySelector(".start__button");
+const Menu = document.querySelector(".menu-container");
 
 
-player.addEventListener("click", e => {
-        e.target.classList.add("player-jump");
-})
+
+
+        easyButton.addEventListener("click", e => {
+                easyButton.classList.add("selected__difficulty");
+                startButtonBlur.classList.remove("start__button_blur")
+                mediumButton.classList.remove("selected__difficulty");
+                hardButton.classList.remove("selected__difficulty");
+                startButtonHover.classList.add("start__button__hover");
+        })
+        mediumButton.addEventListener("click", e => {
+                mediumButton.classList.add("selected__difficulty");
+                startButtonBlur.classList.remove("start__button_blur")
+                easyButton.classList.remove("selected__difficulty");
+                hardButton.classList.remove("selected__difficulty");
+                startButtonHover.classList.add("start__button__hover");
+        })
+        hardButton.addEventListener("click", e => {
+                hardButton.classList.add("selected__difficulty");
+                startButtonBlur.classList.remove("start__button_blur")
+                easyButton.classList.remove("selected__difficulty");
+                mediumButton.classList.remove("selected__difficulty");
+                startButtonHover.classList.add("start__button__hover");
+        })
+        startButton.addEventListener("click", e => {
+                // if(easyButton.classList == "selected__difficulty")
+                Menu.classList.add("start__menu__no__display");
+                console.log('hi')
+                screenClick.addEventListener("click", e => {
+                        if(playerJump.classList == "player-jump"){return}
+                        playerJump.classList.add("player-jump");
+                        setTimeout(function(){
+                        playerJump.classList.remove("player-jump");
+                        },900);})
+        
+        })
