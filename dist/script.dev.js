2b20bafde1e237120a5395f01c4d32b19bce048a
Frank McNulty <frank.mcnulty@nology.io>
2020-10-02 11:27:47 +0100

    background animation

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -1,6 +1,7 @@
 "use strict";
 
-var playerJump = document.querySelector(".player");
+var player = document.querySelector(".player");
+var enemyAnimation = document.querySelector(".enemy");
 var screenClick = document.querySelector(".game-container");
 var easyButton = document.querySelector(".easy");
 var mediumButton = document.querySelector(".medium");
@@ -9,6 +10,25 @@ var startButtonBlur = document.querySelector(".start__button_blur");
 var startButtonHover = document.querySelector(".start__button");
 var startButton = document.querySelector(".start__button");
 var Menu = document.querySelector(".menu-container");
+var sunAnimation = document.querySelector(".sun");
+var moonAnimation = document.querySelector(".moon");
+var backgroundAnimation = document.querySelector(".furthestbackground");
+var cloudsAnimation = document.querySelector(".fa-cloud");
+var endGame = document.querySelector(".sun-animation");
+var info = document.querySelector(".fa-question");
+var openInfo = document.querySelector(".info__modal__show");
+var closeInfo = document.querySelector(".info__modal__hide");
+var enemyRect = enemyAnimation.getBoundingClientRect();
+var playerRect = player.getBoundingClientRect();
+var sunRect = sunAnimation.getBoundingClientRect();
+console.log(playerRect, enemyRect); // Pre game Menu Javascript
+
+info.addEventListener("click", function (e) {
+  openInfo.classList.remove("info__modal__hide");
+});
+closeInfo.addEventListener("click", function (e) {
+  closeInfo.classList.add("info__modal__hide");
+});
 easyButton.addEventListener("click", function (e) {
   easyButton.classList.add("selected__difficulty");
   startButtonBlur.classList.remove("start__button_blur");
@@ -29,20 +49,40 @@ hardButton.addEventListener("click", function (e) {
   easyButton.classList.remove("selected__difficulty");
   mediumButton.classList.remove("selected__difficulty");
   startButtonHover.classList.add("start__button__hover");
-});
+}); // Beginning of game javascript       
+
 startButton.addEventListener("click", function (e) {
   // if(easyButton.classList == "selected__difficulty")
   Menu.classList.add("start__menu__no__display");
-  console.log('hi');
-  playerJump.classList.add("player-roll");
+  player.classList.add("player-roll");
+  moonAnimation.classList.add("moon-animation");
+  setTimeout(function () {
+    {
+      sunAnimation.classList.add("sun-animation");
+    }
+  }, 20000);
+  setTimeout(function () {
+    {
+      moonAnimation.classList.add("moon__remove");
+    }
+  }, 20000);
+  cloudsAnimation.classList.add("fa-cloud-animation");
+  setTimeout(function () {
+    {
+      enemyAnimation.classList.add("enemy-animation");
+    }
+  }, 3000);
+  backgroundAnimation.classList.add("furthestbackground-animation");
   screenClick.addEventListener("click", function (e) {
-    if (playerJump.classList == "player-jump") {
+    if (player.classList == "player-jump") {
       return;
     }
 
-    playerJump.classList.add("player-jump");
+    player.classList.add("player-jump");
     setTimeout(function () {
-      playerJump.classList.remove("player-jump");
-    }, 900);
+      player.classList.remove("player-jump");
+    }, 400);
   });
-});
+}); // endGame.adEventLister('animationend', () => {
+//         console.log('end');
+// })
--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const playerJump = document.querySelector(".player");
+const player = document.querySelector(".player");
+const enemyAnimation = document.querySelector(".enemy");
 const screenClick = document.querySelector(".game-container");
 const easyButton = document.querySelector(".easy");
 const mediumButton = document.querySelector(".medium");
@@ -7,10 +8,29 @@ const startButtonBlur = document.querySelector(".start__button_blur");
 const startButtonHover = document.querySelector(".start__button");
 const startButton = document.querySelector(".start__button");
 const Menu = document.querySelector(".menu-container");
+const sunAnimation = document.querySelector(".sun");
+const moonAnimation = document.querySelector(".moon");
+const backgroundAnimation = document.querySelector(".furthestbackground");
+const cloudsAnimation = document.querySelector(".fa-cloud");
+const endGame = document.querySelector(".sun-animation");
+const info = document.querySelector(".fa-question");
+const openInfo = document.querySelector(".info__modal__show");
+const closeInfo = document.querySelector(".info__modal__hide");
+
+const enemyRect = enemyAnimation.getBoundingClientRect();
+const playerRect = player.getBoundingClientRect();
+const sunRect = sunAnimation.getBoundingClientRect()
+console.log(playerRect, enemyRect);
 
 
 
-
+// Pre game Menu Javascript
+        info.addEventListener("click", e => {
+                openInfo.classList.remove("info__modal__hide");
+        })
+        closeInfo.addEventListener("click", e => {
+                closeInfo.classList.add("info__modal__hide");
+        })
         easyButton.addEventListener("click", e => {
                 easyButton.classList.add("selected__difficulty");
                 startButtonBlur.classList.remove("start__button_blur")
@@ -32,16 +52,32 @@ const Menu = document.querySelector(".menu-container");
                 mediumButton.classList.remove("selected__difficulty");
                 startButtonHover.classList.add("start__button__hover");
         })
+
+// Beginning of game javascript       
         startButton.addEventListener("click", e => {
                 // if(easyButton.classList == "selected__difficulty")
                 Menu.classList.add("start__menu__no__display");
-                console.log('hi')
-                playerJump.classList.add("player-roll");
-                screenClick.addEventListener("click", e => {
-                        if(playerJump.classList == "player-jump"){return}
-                        playerJump.classList.add("player-jump");
+                player.classList.add("player-roll");
+                moonAnimation.classList.add("moon-animation");
+                setTimeout(function(){
+                        {sunAnimation.classList.add("sun-animation");}
+                        },20000);
                         setTimeout(function(){
-                        playerJump.classList.remove("player-jump");
-                        },900);})
-        
+                        {moonAnimation.classList.add("moon__remove");}
+                                },20000);
+                cloudsAnimation.classList.add("fa-cloud-animation");
+                setTimeout(function(){
+                        {enemyAnimation.classList.add("enemy-animation")}
+                        },3000);
+                backgroundAnimation.classList.add("furthestbackground-animation");
+                screenClick.addEventListener("click", e => {
+                        if(player.classList == "player-jump"){return}
+                        player.classList.add("player-jump");
+                        setTimeout(function(){
+                        player.classList.remove("player-jump");
+                        },400);})
+
         })
+        // endGame.adEventLister('animationend', () => {
+        //         console.log('end');
+        // })
